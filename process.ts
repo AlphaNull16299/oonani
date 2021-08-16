af0785a29909749f0710f7d0dccaa8bdeffc5fae
Sueqkjs <sueqk@outlook.jp>
2021-08-16 22:56:39 +0900

    アナルの締まり、ヨシ！

--- process.ts
+++ process.ts
@@ -4,14 +4,27 @@ const webhook: WebhookClient = new WebhookClient({
   url: process.env.webhookUrl as string
 });
 
-webhook.send("botが起動したよ！！！");
+send("botが起動したよ！！！");
 
 process.on("uncaughtExceptionMonitor", (error: Error, origin: string) => {
-  webhook.send(origin + "(Error): " + error.message);
+  send(origin + "(Error): " + error.message);
+  process.exit(127);
 });
 
-process.on("beforeExit", code => {
-  webhook.send("プロセスがクラッシュしました。終了コード:" + code);
+process.on("exit", code => {
+  switch(code) {
+    case 0:
+      send("プロセスが正常終了しました。終了コード:" + code);
+      break;
+    default:
+      send("プロセスがエラー落ちしました。終了コード:" + code);
+  };
 });
+
+function send(content: string) {
+  const date: Date = new Date();
+  const time: string = `[${date.getUTCFullYear()}/${String(date.getUTCMonth()+1).padStart(2, "0")}/${String(date.getUTCDate()).padStart(2, "0")} ${String(date.getUTCHours()+9).padStart(2, "0")}:${String(date.getUTCMinutes()).padStart(2, "0")}]`;
+  webhook.send(time + " " + content);
+}
 
 export default webhook
